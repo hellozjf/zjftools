40e476123133e634e644d5c3d9c09c781f727bc8
hellozjf <908686171@qq.com>
2017-10-18 08:41:10 +0800

    Delete log4j from zjftools

--- src/main/java/com/hellozjf/zjftools/EclipseProject.java
+++ src/main/java/com/hellozjf/zjftools/EclipseProject.java
@@ -2,12 +2,9 @@ package com.hellozjf.zjftools;
 
 import java.io.File;
 
-import org.apache.log4j.Logger;
 
 public class EclipseProject {
 	
-    private static final Logger LOGGER = Logger.getLogger(EclipseProject.class);
-
 	public static String getProjectPath() throws Exception {
 		File directory = new File("");
         String courseFile = directory.getCanonicalPath();
